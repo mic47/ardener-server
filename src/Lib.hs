bae2aa89f34adc431ea3531d7ae5d67c3dff8089
Michal Nánási <michal.nanasi@gmail.com>
2018-04-29 21:13:47 +0000

    Add raw logging + newlines

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,8 +5,11 @@ module Lib
 
 import Data.Functor
 import Data.Maybe
+import Data.UnixTime
+import Foreign.C.Types
+import GHC.Int
 import Network
-import System.IO  
+import System.IO
 import Control.Concurrent
 import Control.Monad.Trans.RWS.Strict
 import Control.Monad.IO.Class
@@ -14,7 +17,7 @@ import Control.Monad.IO.Class
 import LockedFile
 import Types
 
--- TODO (some random notes, not real plan): 
+-- TODO (some random notes, not real plan):
 -- 1. Listen on port, and echo actions, and parse them.
 -- 2. Periodically send some commands.
 -- 3. Store stuff somewhere
@@ -34,11 +37,12 @@ import Types
 -- Desing:
 -- 1. Thread to accepting connections.
 -- 2. For each ardener, have separate threads. This thread manages ardener state, and do all the logging.
--- 3. 
+-- 3.
 
 data ArdenerEnv = ArdenerEnv
   { dataFile :: LockedFile
   , actionFile :: LockedFile
+  , rawTrafficFile :: LockedFile
   , ardenerHostname :: HostName -- TODO: actually log this
   , ardenerPort :: PortNumber -- TODO: actually log this
   }
@@ -54,10 +58,11 @@ ardenerServer = do
   putStrLn $ "Starting listening on port " ++ show port
   dataFile' <- mkLockedFile "dataFile.json"
   actionFile' <- mkLockedFile "actionFile.json"
-  serverLoop (dataFile', actionFile') sock
+  rawTrafficFile' <- mkLockedFile "rawTrafficFile.json"
+  serverLoop (dataFile', actionFile', rawTrafficFile') sock
 
-serverLoop :: (LockedFile, LockedFile) -> Socket -> IO ()
-serverLoop files@(dataFile, actionFile) socket = do
+serverLoop :: (LockedFile, LockedFile, LockedFile) -> Socket -> IO ()
+serverLoop files@(dataFile, actionFile, rawTrafficFile) socket = do
   (handle, ardenerHostname, ardenerPort) <- accept socket
   hSetBuffering handle NoBuffering -- Maybe LineBuffering?
   void $ forkIO $ void $ runRWST
@@ -71,25 +76,34 @@ type ArIO a = RWST ArdenerEnv () ArdenerState IO a
 handleArdener :: Handle -> ArIO ()
 handleArdener handle = do
   line <- liftIO $ hGetLine handle
+  ArdenerEnv{..} <- ask
+  (CTime timestamp) <- utSeconds <$> liftIO getUnixTime
+  liftIO $ lockedFileAppend rawTrafficFile $ mconcat
+    [ show timestamp
+    , ",", ardenerHostname
+    , ",", show ardenerPort
+    , ",", line
+    , "\n"
+    ]
   case (decodeMessage line :: Either String ClientMessage) of
     Left err -> liftIO $ putStrLn $ "Error decoding of message '" ++ line ++ "': " ++ err
     Right msg -> do
       liftIO $ putStrLn $ show msg
-      handleMessage msg
+      handleMessage timestamp msg
   handleArdener handle
 
-handleMessage :: ClientMessage -> ArIO () 
-handleMessage (IdentifySelf newName) = modify $ 
-  \s -> s { ardenerName = Just newName }
-handleMessage (MeasurementLog mLog) = do
+handleMessage :: Int32 -> ClientMessage -> ArIO ()
+handleMessage _ (IdentifySelf newName) = modify $
+  \s -> s { ardenerName = Just $ filter (/= '\r') newName }
+handleMessage timestamp (MeasurementLog mLog) = do
   dataFile' <- dataFile <$> ask
-  nm <- ardenerName <$> get 
-  liftIO $ lockedFileAppend dataFile' 
-    $ fromMaybe "" nm ++ "," ++ show mLog
-handleMessage (ActionLog aLog) = do
+  nm <- ardenerName <$> get
+  liftIO $ lockedFileAppend dataFile'
+    $ show timestamp ++ "," ++ fromMaybe "" nm ++ "," ++ show mLog ++ "\n"
+handleMessage timestamp (ActionLog aLog) = do
   actionFile' <- actionFile <$> ask
   nm <- ardenerName <$> get
   liftIO $ lockedFileAppend actionFile'
-    $ fromMaybe "" nm ++ "," ++ show aLog
-handleMessage (ConfigDump _conf) = return ()
-handleMessage (ConfigUpdated _conf) = return ()
+    $ show timestamp ++ "," ++ fromMaybe "" nm ++ "," ++ show aLog ++ "\n"
+handleMessage _ (ConfigDump _conf) = return ()
+handleMessage _ (ConfigUpdated _conf) = return ()
